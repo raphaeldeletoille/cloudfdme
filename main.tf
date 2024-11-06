6f34453b808bdf0c1a69a314a3cb04e6de21c1e7
raphaeldeletoille <raphael.deletoille@gmail.com>
2024-11-06 11:16:29 +0100

    add

--- main.tf
+++ main.tf
@@ -1,16 +1,21 @@
+# terraform init
+# terraform plan
+# terraform apply
+# terraform fmt
+
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.8.0"
     }
   }
 }
 
 provider "azurerm" {
-    features {
-    }
-    subscription_id = "556b3479-49e0-4048-ace9-9b100efe5b6d"
+  features {
+  }
+  subscription_id = "556b3479-49e0-4048-ace9-9b100efe5b6d"
   # Configuration options
 }
 
@@ -18,3 +23,6 @@ resource "azurerm_resource_group" "rg" {
   name     = "raphaeld"
   location = "West Europe"
 }
+
+#CREER UN STORAGE ACCOUNT DANS VOTRE RESOURCE GROUP EN TERRAFORM
+#FAIRE EN SORTE QUE L ACCESS TIER SOIT EN "COOL" ET PAS EN "HOT"
